cff670ed676a8d78f11c24854b2f0e2a2db485fb
Sheppard <ms04317@heron035.eps.surrey.ac.uk>
2024-11-09 15:11:32 +0000

    trying more

--- MorseCodeZamZam/includes/seven_segment.h
+++ MorseCodeZamZam/includes/seven_segment.h
@@ -33,11 +33,33 @@ static const unsigned int ALL_SEGMENTS[] = {SEGMENT_A, SEGMENT_B, SEGMENT_C, SEG
 
 // You will need to extend the array to include more letters.
 uint8_t values[] = {
-    0b11111100,         // 0
-    0b01100000,         // 1
-    0b11011010,         // 2
-    0b11110010,         // 3
-    0b01100110         // 4
+    0b11101110, //A
+    0b00111111, //B
+    0b10011101, //C
+    0b01111011, //D
+    0b10011111, //E
+    0b10001110, //F
+    0b11110111, //G
+    0b01101110, //H
+    0b00001100, //I
+    0b01111001, //J
+    0b01101110, //K
+    0b00011101, //L
+    0b10101000, //M
+    0b00101010, //N
+    0b11111101, //O
+    0b11001110, //P
+    0b11100110, //Q
+    0b00001010, //R
+    0b10110110, //S
+    0b00011110, //T
+    0b01111100, //V
+    0b00111000, //U
+    0b01010100, //W
+    0b01101110, //X
+    0b01110110, //Y
+    0b11011010, //Z
+    0b11111111, //8
 };
 
 
@@ -57,7 +79,7 @@ void seven_segment_off() {
 }
 
 void seven_segment_show(unsigned int number) {
-    for (unsigned int i = 0; i < 8; i++) {
+    for (unsigned int i = 0; i < 26; i++) {
         unsigned int segmentBit = 1 << (7 - i);
         bool illuminateSegment = (segmentBit & values[number]);
 
--- MorseCodeZamZam/morse_code.c
+++ MorseCodeZamZam/morse_code.c
@@ -3,14 +3,14 @@
  * You are free to edit/create any functions and variables.
  * You can add extra C files if required.
 */
-
-
 #include <stdio.h>
 #include <string.h>
 #include "pico/stdlib.h"
 #include "includes/seven_segment.h"
+#include <time.h>
 
-#define BUTTON_PIN			16	// Pin 21 (GPIO 16)
+#define BUTTON_PIN			15	// Pin 21 (GPIO 16)
+//#define CLOCKS_PER_SEC CLOCKS_PER_SEC 100
 
 // declare global variables e.g., the time when the button is pressed 
 int pressed ;
@@ -21,7 +21,7 @@ int pressed ;
 void decoder();
 
 // check if the button press is a dot or a dash
-void checkButton();
+void checkButton(clock_t start, clock_t end);
 
 int main() {
 	timer_hw->dbgpause = 0;
@@ -38,25 +38,82 @@ int main() {
 	gpio_set_dir(BUTTON_PIN, GPIO_IN);
 	gpio_pull_down(BUTTON_PIN); // Pull the button pin towards ground (with an internal pull-down resistor).
 
-	while (true) {
+	seven_segment_show(26);
+	sleep_ms(500);
+	seven_segment_off();
+	//display welcome message
 
-		while (gpio_get(BUTTON_PIN)){			
-            // record how long the button is pressed
-            // .....
-			printf("This line is a test\n");  // you can remove this line
-			sleep_ms(150); // adjust the sleep_ms as required
-		} 
+	while (true) {
+		clock_t start;
+		clock_t end;
+
+		if (gpio_get(BUTTON_PIN)) {
+			start = clock();
+		} else if (gpio_get(BUTTON_PIN) == false){
+			end = clock();
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		//bool buttonPressed = gpio_get(BUTTON_PIN);
+		/*if (buttonPressed) {
+		*	printf("The button has been pressed");
+		*}
+		*/
+		//int count = 0;
+		// pressed = 0;
+		// while (gpio_get(BUTTON_PIN)){	
+		// 	pressed = 2;
+		// 	//count++;
+        //     // record how long the button is pressed
+        //     // .....
+		// 	printf("This line is a test\n");  // you can remove this line
+		// 	//sleep_ms(150); // adjust the sleep_ms as required
+
+
+		// }
         // check if the button press is a dot or a dash
-		checkButton();
+		checkButton(start, end);
 	}
+	
 }
 
 void decoder(){
     // a function to be implemented
 }
 
-void checkButton(){
+// void checkButton(int count){
+// 	double timeTaken = count / CLOCKS_PER_SEC;
+// 	printf("%f",timeTaken);
+//     // a function to be implemented
+// }
 
-    // a function to be implemented
+void checkButton(clock_t start, clock_t end){
+	 clock_t startv = start;
+	 clock_t endv = end;
+
+	// if (pressed == 1){
+	// 	start = clock();
+	// 	printf("%f \n",(double)start);
+	// }
+	// if (pressed == 0){
+	// 	end = clock();
+	// 	printf("%f \n",(double)end);
+	// }
+	double timeTaken = (double)(endv - startv) / CLOCKS_PER_SEC;
+	printf("%f \n",timeTaken);
 }
 
