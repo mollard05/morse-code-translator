fa720f12389ecccac8eecd91a71b1c03f8f4c3b7
Sheppard <ms04317@heron054.eps.surrey.ac.uk>
2024-11-18 13:04:36 +0000

    finished morse code

--- MorseCodeZamZam/morse_code.c
+++ MorseCodeZamZam/morse_code.c
@@ -6,7 +6,13 @@
 #include <time.h>
 
 #define BUTTON_PIN			15	// Pin 21 (GPIO 16)
-//#define CLOCKS_PER_SEC CLOCKS_PER_SEC 100
+#define R 13
+#define G 12
+#define B 11
+
+#define BRIGHTNESS 25
+#define MAX_COLOUR_VALUE 255
+#define MAX_PWM_LEVEL 65535
 
 // declare global variables e.g., the time when the button is pressed 
 int pressed = 0;
@@ -28,6 +34,7 @@ void printArray();
 void resetArray();
 void addToArray(int value);//1 is dot 2 is dash
 void angelasIfStatement();
+void outputEight();
 
 int main() {
 	timer_hw->dbgpause = 0;
@@ -44,9 +51,11 @@ int main() {
 	gpio_set_dir(BUTTON_PIN, GPIO_IN);
 	gpio_pull_down(BUTTON_PIN); // Pull the button pin towards ground (with an internal pull-down resistor).
 
-	seven_segment_show(26);
-	sleep_ms(500);
-	seven_segment_off();
+    outputEight();
+	// seven_segment_show(26);
+	// sleep_ms(500);
+	// seven_segment_off();
+
 	//display welcome message
 	printf("\nWelcome!\n");
 	
@@ -65,28 +74,14 @@ int main() {
 		}
 
 		checkButton(notPressed); 		
-		//checkButton(notPressed);   
+ 
 		while (gpio_get(BUTTON_PIN)) {
 			pressed = pressed + 1;
 			notPressed = 0;
 			sleep_ms(50);
-
-			// pressed = 2;
-			// //count++;
-            // // record how long the button is pressed
-            // // .....
-			// printf("This line is a test\n");  // you can remove this line
-			// //sleep_ms(150); // adjust the sleep_ms as required
 		}
-		// decoder(pressed);
 
 		decoder(pressed);
- 		
-		//checkButton(notPressed);        
-		// check if the button press is a dot or a dash
-		// if (pressed == 2) {
-		// 	checkButton();
-		// }
 	}
 	
 }
@@ -95,75 +90,32 @@ void decoder(int pressed){
     if (pressed > 0){
 		if (pressed < 5){
 			//dot or dash loop
-
 			printf("\nThis is a dot!");
 			addToArray(1);
-		} else {
+		} else if (pressed < 14){
 			printf("\nThis is a dash!");
 			addToArray(2);
-		}
+		} else {
+            outputEight();
+            printf("\nSignal not recognised - too long!");
+            resetArray();
+            //if button pressed for too long, outputs an error
+        }
 	}
 	// a function to be implemented
 }
 
-// void checkButton(int count){
-// 	double timeTaken = count / CLOCKS_PER_SEC;
-// 	printf("%f",timeTaken);
-//     // a function to be implemented
-// }
-
 void checkButton(int notPressed){
 	if (notPressed > 8){
 		printf("\nThis is an inter-letter gap!");
-		//make array all entities 0
 		angelasIfStatement();
 		resetArray();
+        //removes all values of array
 	} else {
-		
-
-
 		printf("\nThis is a inter_signal gap!");
 	}
 
-	// clock_t start;
-	// clock_t end;
-	// if (pressed == 2){
-	// 	start = clock();
-	// 	printf("%f \n",(double)start);
-	// }
-	// if (pressed == 1){
-	// 	end = clock();
-	// 	printf("%f \n",(double)end);
-	// }
-	// double timeTaken = (double)(end - start) / CLOCKS_PER_SEC;
-	// printf("The time taken was %f \n",timeTaken);
 }
-
-/*
-int pressed;
-int not_pressed;
-need a letter array to add the dots and dashes
-string word to put letters into
-
-while (true) {
-  
-  while (gpio_get(BUTTON_PIN)){
-      // record how long the button is pressed
-      pressed = pressed +1;
-      not_pressed = 0;
-      sleep_ms(50); // adjust the sleep_ms as required
-      // .....
-  }
-// ....... 
-// more to be done ...
-	not pressed = 1
-	addsignal to letter array
-	pressed = 0 
-	sleep(50) 
-	..
-	if slept for 700ms pass to letter function
-
-*/
 
 int countElement() {
 	//return the last free value
@@ -195,139 +147,173 @@ void addToArray(int value) {
 	letterArray[countElement()] = value;
 }
 
+void outputEight() {
+    seven_segment_show(26);
+	sleep_ms(500);
+	seven_segment_off();
+}
+
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++Angela's if statement++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 void angelasIfStatement() {
 	//Angela's if statements 
+    bool isCorrect = false;
 	int count = countElement();
 	printf("%d ",count);
-  	if (letterArray[0] = 0){
-       printf("Array empty :((()))");
-       //pass to subroutine to reset
-  	}
-  	else if (count == 1) {
+  	
+    if (count == 1) {
+        printf("\nzoe");
+        printf("\n%d",letterArray[0]);
   	    if (letterArray[0] == 1) {
-           seven_segment_show(4);
-           // calls E
-		   printf("This is an E");
+            isCorrect = true; 
+            seven_segment_show(4);
+            // calls E
   	    } else if (letterArray[0] == 2) {
-           seven_segment_show(19);
-           // calls T
+            isCorrect = true;
+            seven_segment_show(19);
+            // calls T
        }
    }
    else if (count == 2) {
-//       if (letterArray == [1,2]) {
        if (letterArray[0] == 1 && letterArray[1] == 2) {
-           seven_segment_show(0);
-           // calls A
+            isCorrect = true;
+            seven_segment_show(0);
+            // calls A
        }
        if (letterArray[0] == 1 && letterArray[1] == 1) {
-           printf("Dis is an I");
-		   seven_segment_show(8);
-           // calls I
+            isCorrect = true;
+		    seven_segment_show(8);
+            // calls I
        }
        if (letterArray[0] == 2 && letterArray[1] == 2) {
-           seven_segment_show(12);
-           // calls M
+            isCorrect = true;
+            seven_segment_show(12);
+            // calls M
        }
        if (letterArray[0] == 2 && letterArray[1] == 1) {
-           seven_segment_show(13);
-           // calls N
+            isCorrect = true;
+            seven_segment_show(13);
+            // calls N
        }
     }
     else if (count == 3){
        if (letterArray[0] == 2 && letterArray[1] == 1 && letterArray[2] == 1) {
-           seven_segment_show(3);
-           // calls D
+            isCorrect = true;
+            seven_segment_show(3);
+            // calls D
        }
        if (letterArray[0] == 2 && letterArray[1] == 2 && letterArray[2] == 1) {
-           seven_segment_show(6);
-           // calls G
+            isCorrect = true;
+            seven_segment_show(6);
+            // calls G
        }
        if (letterArray[0] == 2 && letterArray[1] == 1 && letterArray[2] == 2) {
-           seven_segment_show(10);
-           // calls K
+            isCorrect = true;
+            seven_segment_show(10);
+            // calls K
        }
        if (letterArray[0] == 2 && letterArray[1] == 2 && letterArray[2] == 2) {
-           seven_segment_show(14);
-           // calls O
+            isCorrect = true;
+            seven_segment_show(14);
+            // calls O
        }
        if (letterArray[0] == 1 && letterArray[1] == 2 && letterArray[2] == 1) {
-           seven_segment_show(17);
-           // calls R
+            isCorrect = true;
+            seven_segment_show(17);
+            // calls R
        }
        if (letterArray[0] == 1 && letterArray[1] == 1 && letterArray[2] == 1) {
-           seven_segment_show(18);
-           // calls S
+            isCorrect = true;
+            seven_segment_show(18);
+            // calls S
        }
        if (letterArray[0] == 1 && letterArray[1] == 1 && letterArray[2] == 2) {
-           seven_segment_show(20);
-           // calls U
+            isCorrect = true;
+            seven_segment_show(20);
+            // calls U
        }
        if (letterArray[0] == 1 && letterArray[1] == 2 && letterArray[2] == 2) {
-           seven_segment_show(22);
-           // calls W
+            isCorrect = true;
+            seven_segment_show(22);
+            // calls W
        }
     }
     else if (count == 4){
        if (letterArray[0] == 2 && letterArray[1] == 1 && letterArray[2] == 1 && letterArray[3] == 1) {
-           seven_segment_show(1);
-           // calls B
+            isCorrect = true;
+            seven_segment_show(1);
+            // calls B
        }
        if (letterArray[0] == 2 && letterArray[1] == 1 && letterArray[2] == 2 && letterArray[3] == 1) {
-           seven_segment_show(2);
-           // calls C
+            isCorrect = true;
+            seven_segment_show(2);
+            // calls C
        }
        if (letterArray[0] == 1 && letterArray[1] == 1 && letterArray[2] == 2 && letterArray[3] == 1) {
-           seven_segment_show(5);
-           // calls F
+            isCorrect = true;
+            seven_segment_show(5);
+            // calls F
        }
        if (letterArray[0] == 1 && letterArray[1] == 1 && letterArray[2] == 1 && letterArray[3] == 1) {
-           seven_segment_show(7);
-           // calls H
+            isCorrect = true;
+            seven_segment_show(7);
+            // calls H
        }
        if (letterArray[0] == 1 && letterArray[1] == 2 && letterArray[2] == 2 && letterArray[3] == 2) {
-           seven_segment_show(9);
-           // calls J
+            isCorrect = true;
+            seven_segment_show(9);
+            // calls J
        }
        if (letterArray[0] == 1 && letterArray[1] == 2 && letterArray[2] == 1 && letterArray[3] == 1) {
-           seven_segment_show(11);
-           // calls L
+            isCorrect = true;
+            seven_segment_show(11);
+            // calls L
        }
        if (letterArray[0] == 1 && letterArray[1] == 2 && letterArray[2] == 2 && letterArray[3] == 1) {
-           seven_segment_show(15);
-           // calls P
+            isCorrect = true;
+            seven_segment_show(15);
+            // calls P
        }
        if (letterArray[0] == 2 && letterArray[1] == 2 && letterArray[2] == 1 && letterArray[3] == 2) {
-           seven_segment_show(16);
-           // calls Q
+            isCorrect = true;
+            seven_segment_show(16);
+            // calls Q
        }
        if (letterArray[0] == 1 && letterArray[1] == 1 && letterArray[2] == 1 && letterArray[3] == 2) {
-           seven_segment_show(21);
-           // calls V
+            isCorrect = true;
+            seven_segment_show(21);
+            // calls V
        }
        if (letterArray[0] == 2 && letterArray[1] == 1 && letterArray[2] == 1 && letterArray[3] == 2) {
-           seven_segment_show(23);
-           // calls X
+            isCorrect = true;
+            seven_segment_show(23);
+            // calls X
        }
        if (letterArray[0] == 2 && letterArray[1] == 1 && letterArray[2] == 2 && letterArray[3] == 2) {
-           seven_segment_show(24);
-           // calls Y
+            isCorrect = true;
+            seven_segment_show(24);
+            // calls Y
        }
        if (letterArray[0] == 2 && letterArray[1] == 2 && letterArray[2] == 1 && letterArray[3] == 1) {
-           seven_segment_show(25);
-           // calls Z
+            isCorrect = true;
+            seven_segment_show(25);
+            // calls Z
        }
    } else if (letterArray[0] == 0 && letterArray[1] == 0 && letterArray[2] == 0 && letterArray[3] == 0) {
-	printf("\nNothing has been inputted right now!");
-   } else {
+	    printf("\nNothing has been inputted right now!");
+        isCorrect = true;
+   } 
+
+   if (isCorrect == false) {
        printf("\nError - invalid input");
-       seven_segment_show(26);
+       outputEight();
        // displays error
+   }
 	   
+   if (letterArray[0] != 0){
+    sleep_ms(500); 
+    //displays letter for longer if something has been inputted
    }
    sleep_ms(50);
    seven_segment_off();
 }
-//after a letter has been inputted can be paused for sleep_ms(500) - wont be adding another letter when a letter is being displayed 
-//the if statements should be else ifs, the final else is not a count of 4 
